068d59cb4eee6397bc62e964f5057bf2e3167efa
berenar <bernatpericasserra97@gmail.com>
2021-08-08 00:28:47 +0200

    Added toggle switch with no functionality

--- app/src/components/ThemeChanger.tsx
+++ app/src/components/ThemeChanger.tsx
@@ -1,17 +1,26 @@
 import * as R from 'react';
 import * as SVG from '../assets/svgs';
-
+import Switch from 'react-switch';
 interface Props {
   className: string;
   light: boolean;
 }
 
 export const ThemeChanger = (props: Props): R.ReactElement => {
+  const [lights, setLights] = R.useState(true);
   return (
     <div className={props.className}>
-      <SVG.Moon fillColor="black" />
-      <div>switch</div>
-      <SVG.Sun fillColor="black" />
+      <SVG.Moon color="black" />
+      <Switch
+        className="toggleSwitch"
+        offColor="#373838"
+        onColor="#373838"
+        checkedIcon={false}
+        uncheckedIcon={false}
+        onChange={(v) => setLights(v)}
+        checked={lights}
+      />
+      <SVG.Sun color="black" />
     </div>
   );
 };
