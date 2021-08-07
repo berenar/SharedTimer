bca9075fe36ba385e4715028466eb442ce7794f8
berenar <bernatpericasserra97@gmail.com>
2021-08-07 21:48:21 +0200

    Update(fetch) the shared time when pausing local time

--- app/src/components/Timers.tsx
+++ app/src/components/Timers.tsx
@@ -13,19 +13,26 @@ export const Timers = (props: Props): R.ReactElement => {
   const [sharedTime, setSharedTime] = R.useState('00:00:00');
 
   R.useEffect(() => {
-    async function fetch() {
-      const timeInMs: number = await Api.fetchTime();
-      const timeInHuman: string = Utils.millisecondsToHuman(timeInMs);
-      setSharedTime(timeInHuman);
-    }
-    fetch();
+    fetchSharedTime();
   });
+
+  const fetchSharedTime = async () => {
+    const timeInMs: number = await Api.fetchTime();
+    const timeInHuman: string = Utils.millisecondsToHuman(timeInMs);
+    setSharedTime(timeInHuman);
+  };
+
+  const stopLocalTimeHandler = async (elapsed: number) => {
+    const updatedTime = await Api.incrementTime(elapsed);
+    const timeInHuman: string = Utils.millisecondsToHuman(updatedTime);
+    setSharedTime(timeInHuman);
+  };
 
   return (
     <div className={props.className}>
       <div className="sharedTimer">{sharedTime}</div>
       <div className="localTimer">
-        <Stopwatch addToTotal={(elapsed) => Api.incrementTime(elapsed)} />
+        <Stopwatch addToTotal={(elapsed) => stopLocalTimeHandler(elapsed)} />
       </div>
     </div>
   );
