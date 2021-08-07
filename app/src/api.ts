f044533fee0ace6a0bd79e6cdb44d8fb921736fd
berenar <bernatpericasserra97@gmail.com>
2021-08-07 21:30:42 +0200

    DRY refactored api wrappers

--- app/src/api.ts
+++ app/src/api.ts
@@ -1,10 +1,31 @@
 import * as Types from './types';
 
 const url = 'http://localhost:3001/timer/api/v1/main'; //TODO: hardcoded
+interface CallOptions {
+  method: Types.HttpVerbs;
+  headers?: Headers;
+  body?: { ms: number };
+}
 
 export const fetchTime = async (): Promise<number> => {
-  const options = { method: Types.HTTPverbs.Get };
-  const response = await fetch(`${url}/current`, options);
+  const options: CallOptions = { method: Types.HttpVerbs.Get };
+  return await call('current', options);
+};
+
+export const incrementTime = async (elapsed: number): Promise<number> => {
+  const putHeaders = new Headers();
+  putHeaders.append('Content-Type', 'application/json');
+  const options = {
+    method: 'PUT',
+    headers: putHeaders,
+    body: JSON.stringify({ ms: elapsed }),
+  };
+  return await call('current', options);
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const call = async (resource: string, options: any): Promise<number> => {
+  const response = await fetch(`${url}/${resource}`, options);
   const body: Types.StandardResponse = await response.json();
   if (body.status != 'OK') return 0;
   if (typeof body.data === 'number') return body.data;
